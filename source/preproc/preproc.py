6c7ce07362f332cf2b4626aac26df2089cb0c3a1
swyang50066 <gog500@naver.com>
2022-03-12 16:40:08 +0900

    change to snake style and rearrange internal functions

--- source/preproc/preproc.py
+++ source/preproc/preproc.py
@@ -22,30 +22,11 @@ DEFAULT_USER_DEFINED_FEATURES = {
 }
 
 
-def loadDataset(filename):
-    ''' Return dataframe by loading csv dataset file (.csv)
-    '''
-    return pd.read_csv(filename)
-
-
-def loadBatchFromDataFame(df, start=20090101, end=20201231):
-    ''' Return a batch data from whole dataframe, 
-        ranging between 'start' and 'end'
-    '''
-    # Extract batch and sort items in order of 'date'
-    batch = df[(df.datadata >= start) & (df.date < end)]
-    batch = batch.sort_values(by=["date", "tic"])
-    batch = batch.reset_index(drop=True)    # reindexing
-
-    return batch
-
-
-def convertAdjustedToOHLCV(df):
+def _from_adjusted_to_ohlcv(df):
     ''' Convert Adjusted values to OHLCV
     '''
     # Fetch adjusted items
-    x = df.copy()
-    x = x[ADUSTED_VALUE_HEADS]
+    x = df.copy()[ADUSTED_VALUE_HEADS]
 
     # Fill non-zero daily adjustment factor
     x["ajexdi"] = x["adjexdi"].apply(
@@ -67,12 +48,11 @@ def convertAdjustedToOHLCV(df):
     return x
 
 
-def convertOHLCVToAdjusted(df):
+def _from_ohlcv_to_adjusted(df):
     ''' Convert OHLCV values to Adjusted values
     '''
     # Fetch OHLCV items
-    x = df.copy()
-    x = x[OHLCV_VALUE_HEADS]
+    x = df.copy()[OHLCV_VALUE_HEADS]
 
     # Fill non-zero daily adjustment factor
     x["ajexdi"] = x["adjexdi"].apply(
@@ -87,13 +67,13 @@ def convertOHLCVToAdjusted(df):
     x["schtrd"] = x["volume"]
 
     # Sort items
-    x = x[OHLCV_VALUE_HEADS]
+    x = x[ADJUSTED_VALUE_HEADS]
     x = x.sort_values(by=["date", "tic"])
     x = x.reset_index(drop=True)    # reindexing
 
     return x
 
-def addUserDefinedFeature(df, features=DEFAULT_USER_DEFINED_FEATURES):
+def add_user_defined_feature(df, features):
     ''' Append 'user-defined-feature' to the data frame
 
         *** To be updated soon!
@@ -101,14 +81,12 @@ def addUserDefinedFeature(df, features=DEFAULT_USER_DEFINED_FEATURES):
     # Fetch data frame
     x = df.copy()
     
-    # ====>
     x["daily_return"] = df.cloase.pct_change(1)
-    # <====
 
     return x
 
 
-def addVolatilityIndex(df):
+def add_volatility_index(df):
     ''' Append 'Volatility Index (VIX)' to the data frame 
     '''
     # Fetch data frame 
@@ -134,7 +112,7 @@ def addVolatilityIndex(df):
     return x
 
 
-def addTechnicalIndicator(df, indicators=DEFAULT_TECHNICAL_INDICATOR_LIST):
+def add_technical_indicator(df, indicators):
     ''' Calculate technical indicators and append it to the dataframe
     '''
     # Switch columns
@@ -175,25 +153,7 @@ def addTechnicalIndicator(df, indicators=DEFAULT_TECHNICAL_INDICATOR_LIST):
     return x
 
 
-def addTurbulenceIndex(df):
-    ''' Calculate turbulence index and append it to the dataframe
-        (The index is based on dow 30)
-    '''
-    # Fetch data frame
-    x = df.copy()
- 
-    # Calculate turbulence index   
-    turbulence = calcTurbulenceIndex(x)
-    
-    # Merge and sort items
-    x = x.merge(turbulence, on="date")
-    x = x.sort_values(['date','tic'])
-    x = x.reset_index(drop=True)
-    
-    return x
-
-
-def calcTurbulenceIndex(self, data, start=252):
+def get_turbulence_index(self, data, start=252):
     ''' Calculate financial turbulence index
         
         (Consider after a year, default: start=252)
@@ -207,12 +167,15 @@ def calcTurbulenceIndex(self, data, start=252):
         
     # start after a year
     turbulence = [0] * start
-        
+    
     count = 0
     for i in range(start, len(unique_date)):
-        current_price = df_price_pivot[df_price_pivot.index == unique_date[i]]
+        # Get current price
+        current_price = df_price_pivot[
+            df_price_pivot.index == unique_date[i]
+        ]
             
-        # use one year rolling window to calcualte covariance
+        # Use one year rolling window to calcualte covariance
         hist_price = df_price_pivot[
             (df_price_pivot.index < unique_date[i])
             & (df_price_pivot.index >= unique_date[i - 252])
@@ -238,13 +201,14 @@ def calcTurbulenceIndex(self, data, start=252):
             if count > 2:
                 turbulence_temp = temp[0][0]
             else:
-                # avoid large outlier because of the calculation just begins
+                # Avoid large outlier because of the calculation just begins
                 turbulence_temp = 0
         else:
             turbulence_temp = 0
         
         turbulence.append(turbulence_temp)
 
+    # Return a set of tubulence index
     turbulence = pd.DataFrame(
         {"date": df_price_pivot.index, "turbulence": turbulence}
     )
@@ -252,13 +216,31 @@ def calcTurbulenceIndex(self, data, start=252):
     return turbulence
 
 
+def add_turbulence_index(df):
+    ''' Calculate turbulence index and append it to the dataframe
+        (The index is based on dow 30)
+    '''
+    # Fetch data frame
+    x = df.copy()
+
+    # Calculate turbulence index
+    turbulence = get_turbulence_index(x)
+
+    # Merge and sort items
+    x = x.merge(turbulence, on="date")
+    x = x.sort_values(['date','tic'])
+    x = x.reset_index(drop=True)
+
+    return x
+
+
 class Preprocessor(object):
     def __init__(self, TECHNICAL_INDICATOR_LIST=list(),
                        USER_DEFINED_FEATURES=dict(), 
-                       bUseTechnicalIndicator=True,
-                       bUseVolatilityIndex=True,
-                       bUseTurbulenceIndex=True,
-                       bUseUserDefinedIndex=True 
+                       b_use_technical_indicator=True,
+                       b_use_volatility_index=True,
+                       b_use_turbulence_index=True,
+                       b_use_user_defined_index=True 
                 ):
         # Declare parameters
         if (isinstance(TECHNICAL_INDICATOR_LIST, list) and 
@@ -272,48 +254,63 @@ class Preprocessor(object):
         else:
             self.USER_DEFINED_FEATURES = DEFAULT_USER_DEFINED_FEATURES
 
-        self.bUseTechnicalIndicator = bUseTechnicalIndicator
-        self.bUseVolatilityIndex = bUseVolatilityIndex
-        self.bUseTurbulenceIndex = bUseTurbulenceIndex
-        self.bUseUserDefinedIndex = bUseUserDefinedIndex
+        self.b_use_technical_indicator = b_use_technical_indicator
+        self.b_use_volatility_index = b_use_volatility_index
+        self.b_use_turbulence_index = b_use_turbulence_index
+        self.b_use_user_defined_index = b_use_user_defined_index
 
-    def load(self, DATASET_FILE_NAME="dow30_2009_to_2020.csv"):
-        ''' Load financial dataset
+    def load_yahoo_finance(self):
+        ''' Load Yahoo finance dataset
         '''
-        # ====> Update Yahoo Finance dataset loader
+        pass
+    
+    def load_csv(self, filename="dow30_2009_to_2020.csv"): 
+        ''' Return dataframe by loading csv dataset file (.csv)
+        '''
         # Set path of data csv file
-        CSV_FILE_PATH = (
+        filepath = (
             os.path.dirname(os.path.realpath(__file__))
             + "../assets/"
-            + DATASET_FILE_NAME
+            + dataset_file_name
         )
 
-        return pd.DataFrame(CSV_FILE_PATH)
+        return pd.read_csv(filepath)
+
+    def batch(df, start_date=20090101, end_date=20201231):
+        ''' Return a batch data from whole dataframe,
+            of which being between 'start_date' and 'end_date'
+        '''
+        # Extract batch and sort items in order of 'date'
+        df_batch = df[(df.datadata >= start_date) & (df.date < end_date)]
+        df_batch = df_batch.sort_values(by=["date", "tic"])
+        df_batch = df_batch.reset_index(drop=True)    # reindexing
+
+        return df_batch
 
     def apply(self, df):
         ''' Apply preprocessing
         '''
         # Add technical indicators using stockstats
         print("Append technical indicators")
-        if self.bUseTechnicalIndicator:
-            df = addTechnicalIndicator(
+        if self.b_use_technical_indicator:
+            df = add_technical_indicator(
                 df, indicators=self.TECHNICAL_INDICATOR_LIST
             )
 
         # Add VIX for multiple stock
         print("Append volatility index (VIX)")
-        if self.bUseVolatilityIndex:
-            df = addVolatilityIndex(df)
+        if self.b_use_volatility_index:
+            df = add_volatility_index(df)
 
         # Add turbulence index for multiple stock
         print("Append turbulence index")
-        if self.bUseTurbulence:
-            df = addTurbulenceIndex(df)
+        if self.b_use_turbulence_index:
+            df = add_turbulence_index(df)
 
         # Add user defined feature
         print("Append user-defined-feature")
-        if self.bUseUserDefinedFeature:
-            df = addUserDefinedFeature(
+        if self.b_use_user_defined_feature:
+            df = add_user_defined_feature(
                 df, features=self.USER_DEFINED_FEATURES
             )
 
