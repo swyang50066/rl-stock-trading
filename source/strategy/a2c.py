da6f8f4c96f77bab92b3dfbea64ec1f2a258235e
swyang50066 <gog500@naver.com>
2022-03-14 20:07:39 +0900

    A2C algorithm updated

--- source/strategy/a2c.py
+++ source/strategy/a2c.py
@@ -1,198 +1,208 @@
-import numpy as np
-import keras.backend as K
+import  numpy       as  np
 
-from keras.models import Model
-from keras.layers import Input, Dense, Flatten
-from keras.optimizers import Adam
-from .agent import Agent
+import  keras.backend       as  K
+from    keras.models        import  Model
+from    keras.layers        import  Input, Dense, Flatten
+from    keras.optimizers    import  RMSprop
+from    keras.regularizers  import  l2 
+from    keras.utils         import  to_categorical
 
-class Actor(Agent):
-    """ Actor for the A2C Algorithm
-    """
-
-    def __init__(self, inp_dim, out_dim, network, lr):
-        Agent.__init__(self, inp_dim, out_dim, lr)
-        self.model = self.addHead(network)
-        self.action_pl = K.placeholder(shape=(None, self.out_dim))
-        self.advantages_pl = K.placeholder(shape=(None,))
-    def addHead(self, network):
-        """ Assemble Actor network to predict probability of each action
-        """
-        x = Dense(128, activation='relu')(network.output)
-        out = Dense(self.out_dim, activation='softmax')(x)
-        return Model(network.input, out)
-
-    def optimizer(self):
-        """ Actor Optimization: Advantages + Entropy term to encourage exploration
-        (Cf. https://arxiv.org/abs/1602.01783)
-        """
-        weighted_actions = K.sum(self.action_pl * self.model.output, axis=1)
-        eligibility = K.log(weighted_actions + 1e-10) * K.stop_gradient(self.advantages_pl)
-        entropy = K.sum(self.model.output * K.log(self.model.output + 1e-10), axis=1)
-        loss = 0.001 * entropy - K.sum(eligibility)
-
-        updates = self.rms_optimizer.get_updates(self.model.trainable_weights, [], loss)
-        return K.function([self.model.input, self.action_pl, self.advantages_pl], [], updates=updates)
-
-    def save(self, path):
-        self.model.save_weights(path + '_actor.h5')
-
-    def load_weights(self, path):
-        self.model.load_weights(path)
+from    strategy.agent      import  Agent, Strategy
 
 
-class Critic(Agent):
-    """ Critic for the A2C Algorithm
-    """
+class A2CAgent(Agent):
+    '''  Actor for the A2C algorithm
+    '''
+    def __init__(self, input_dim, output_dim, lr, gamma):
 
-    def __init__(self, inp_dim, out_dim, network, lr):
-        Agent.__init__(self, inp_dim, out_dim, lr)
-        self.model = self.addHead(network)
-        self.discounted_r = K.placeholder(shape=(None,))
+        # Build network model
+        inputs = self.build(input_dim=input_dim, output_dim=output_dim)
 
-    def addHead(self, network):
-        """ Assemble Critic network to predict value of each state
-        """
-        x = Dense(128, activation='relu')(network.output)
-        out = Dense(1, activation='linear')(x)
-        return Model(network.input, out)
+        # Set up model functions
+        self.setup(*(inputs + (lr,)))
 
-    def optimizer(self):
-        """ Critic Optimization: Mean Squared Error over discounted rewards
-        """
-        critic_loss = K.mean(K.square(self.discounted_r - self.model.output))
-        updates = self.rms_optimizer.get_updates(self.model.trainable_weights, [], critic_loss)
-        return K.function([self.model.input, self.discounted_r], [], updates=updates)
+        # Compile network model with loss function and optimizer
+        self.compile()
 
-    def save(self, path):
-        self.model.save_weights(path + '_critic.h5')
+    def _policy_loss_function(value_true, value_pred, eps=1.e-16):
+        ''' Policy loss function
+        '''
+        advantage = value_true - value_pred
+        def _loss(y_true, y_pred):
+            # Eligibility
+            eligibility = K.sum(y_pred*y_true, axis=1, keepdims=True) 
+            eligibility = K.sum(-K.log(eligibility) + eps)
+            eligibility *= K.stop_gradient(advantage))
+            
+            # Entropy
+            entropy = K.sum(y_pred*K.log(y_pred + eps), axis=1)
+            
+            return alpha*entropy - eligibility
+        
+        return _loss
 
-    def load_weights(self, path):
-        self.model.load_weights(path)
+    def _value_loss_function(y_true, y_pred):
+        adventage = y_true - y_pred
+        return K.mean(.5*K.square(adventage))
+
+    def setup(self, input_value, output_value, lr):
+        ''' Set optimizer, loss and callback functions
+        ''' 
+        # Set optimizer
+        self.optimizer = RMSprop(lr=lr, epsilon=0.1, rho=0.99)
+
+        # Define loss function
+        self.loss = [
+            self._pol_loss_func(input_value, output_value)
+            self._val_loss_func, 
+        ]
+
+        # List callbacks
+        self.callback = []
+
+    def build(self, input_dim, output_dim):
+        ''' Build network model
+        '''
+        # Inputs
+        input_state = Input(shape=(input_dim,))
+        input_value = Input(shape=(1,))
+
+        # Shared feed-forward network
+        x = Flatten()(input_state)
+        x = Dense(64, activation="relu")(x)
+        x = Dense(128, activation="relu")(x)
+
+        # Actor pathway
+        output_action = Dense(128, activation="relu")(x)
+        output_action = Dense(output_dim, activation="softmax")(output_action)
+
+        # Critic pathway 
+        output_value = Dense(128, activation="relu")(bn)
+        output_value = Dense(1, activation="linear")(output_value)
+
+        # Build model        
+        self.model = Model(
+            inputs=[inputs_state, input_value],
+            outputs=[output_action, output_value]
+        )
+
+        return input_value, output_value
+
+    def compile(self, ):
+        ''' Compile model with optimizer and loss function
+        '''
+        self.model.compile(loss=self.loss, optimizer=self.optimizer)
+
+    def evolve(self, transitions):
+        ''' Evolve model for a iteration
+        '''
+        # Parse input variables
+        states, actions, discounted_reward, _  = transitions
+
+        # Evaluate input and target values
+        inputs = [states, discounted_reward]
+        targets = [actions, self.model.predict(states)]
+
+        # Train on batch
+        self.model.train_on_batch(x=inputs, y=outputs)
 
 
-import random
-import numpy as np
+class A2C(Strategy):
+    ''' Actor-Critic algorithm
+    '''
+    def __init__(self, env,
+                       input_dim=181,
+                       output_dim=3,  
+                       gamma=.99,
+                       init_learning_rate=1.e-4,
+                       num_frame=4,
+                       num_episode=5000,
+                ) -> None: 
+        # Initialize internal setup
+        super(Strategy, self).__init__(self, 
+            input_dim=input_dim, 
+            output_dim=output_dim, 
+            gamma=gamma,
+            init_learning_rate=init_learning_rate,
+            num_frame=num_frame,
+            num_episode=num_episode
+        )
 
-from tqdm import tqdm
-from keras.models import Model
-from keras import regularizers
-from keras.utils import to_categorical
-from keras.layers import Input, Dense, Flatten
+        # Environment object
+        self.env = env
 
-from .critic import Critic
-from .actor import Actor
-from utils.networks import tfSummary
-from utils.stats import gather_stats
-
-class A2C:
-    """ Actor-Critic Main Algorithm
-    """
-
-    def __init__(self, act_dim, env_dim, k, gamma = 0.99, lr = 0.0001):
-        """ Initialization
-        """
-        # Environment and A2C parameters
-        self.act_dim = act_dim
-        self.env_dim = (k,) + env_dim
-        self.gamma = gamma
-        self.lr = lr
         # Create actor and critic networks
-        self.shared = self.buildNetwork()
-        self.actor = Actor(self.env_dim, act_dim, self.shared, lr)
-        self.critic = Critic(self.env_dim, act_dim, self.shared, lr)
-        # Build optimizers
-        self.a_opt = self.actor.optimizer()
-        self.c_opt = self.critic.optimizer()
+        self.agent = A2CAgent(
+            input_dim=self.input_dim, 
+            output_dim=self.output_dim, 
+            lr=self.init_learning_rate,
+            gamma=self.gamma
+        )
+        
+    def get_next_action(self, state):
+        ''' Use actor policy to get next action
+        '''
+        return np.random.choice(
+            np.arange(self.output_dim), 
+            size=1, 
+            p=self.actor.predict(state).ravel()
+        )[0]
 
-    def buildNetwork(self):
-        """ Assemble shared layers
-        """
-        inp = Input((self.env_dim))
-        x = Flatten()(inp)
-        x = Dense(64, activation='relu')(x)
-        x = Dense(128, activation='relu')(x)
-        return Model(inp, x)
+    def get_discounted_reward(self, rewards):
+        ''' Evaluate discounted reward
+        '''
+        discounted_reward = 0
+        for reward in reversed(rewards):
+            discounted_reward = reward + self.gamma*discounted_reward
 
-    def policy_action(self, s):
-        """ Use the actor to predict the next action to take, using the policy
-        """
-        return np.random.choice(np.arange(self.act_dim), 1, p=self.actor.predict(s).ravel())[0]
+        return discounted_reward
 
-    def discount(self, r):
-        """ Compute the gamma-discounted rewards over an episode
-        """
-        discounted_r, cumul_r = np.zeros_like(r), 0
-        for t in reversed(range(0, len(r))):
-            cumul_r = r[t] + cumul_r * self.gamma
-            discounted_r[t] = cumul_r
-        return discounted_r
-
-    def train_models(self, states, actions, rewards, done):
-        """ Update actor and critic networks from experience
-        """
-        # Compute discounted rewards and Advantage (TD. Error)
-        discounted_rewards = self.discount(rewards)
-        state_values = self.critic.predict(np.array(states))
-        advantages = discounted_rewards - np.reshape(state_values, len(state_values))
-        # Networks optimization
-        self.a_opt([states, actions, advantages])
-        self.c_opt([states, discounted_rewards])
-
-    def train(self, env, args, summary_writer):
-        """ Main A2C Training Algorithm
-        """
-
-        results = []
-
-        # Main Loop
-        tqdm_e = tqdm(range(args.nb_episodes), desc='Score', leave=True, unit=" episodes")
-        for e in tqdm_e:
-
+    def train(self):
+        ''' Train agent under A2C algorithm
+        '''
+        for episode in range(self.num_episode):
             # Reset episode
-            time, cumul_reward, done = 0, 0, False
-            old_state = env.reset()
-            actions, states, rewards = [], [], []
+            curr_state = self.env.reset()
+            actions, states, rewards, b_terminal = [], [], [], False
 
-            while not done:
-                if args.render: env.render()
-                # Actor picks an action (following the policy)
-                a = self.policy_action(old_state)
-                # Retrieve new state, reward, and whether the state is terminal
-                new_state, r, done, _ = env.step(a)
+            # Simulate episode
+            while not b_terminal:
+                # Actor an action by following the current policy
+                action = self.get_next_action(curr_state)
+                
+                # Retrieve next state and reward
+                next_state, reward, b_terminal, _ = self.env.step(action)
+                
                 # Memorize (s, a, r) for training
-                actions.append(to_categorical(a, self.act_dim))
-                rewards.append(r)
-                states.append(old_state)
+                actions.append(to_categorical(action, self.output_dim))
+                rewards.append(reward)
+                states.append(curr_state)
+                
                 # Update current state
-                old_state = new_state
-                cumul_reward += r
-                time += 1
+                curr_state = next_state
 
-            # Train using discounted rewards ie. compute updates
-            self.train_models(states, actions, rewards, done)
+            # Evolve agents
+            discounted_reward = self.get_discounted_reward(rewards)
+            transitions = (states, actions, discounted_reward, b_terminal)
+            self.evolve(transitions)    
 
-            # Gather stats every episode for plotting
-            if(args.gather_stats):
-                mean, stdev = gather_stats(self, env)
-                results.append([e, mean, stdev])
+    def trade(self):
+        ''' Start stock trade
+        '''
+        # Initialize episode
+        curr_state = self.env.reset()
+        portfolio = list()    ## This is going to be substitute a class method
 
-            # Export results for Tensorboard
-            score = tfSummary('score', cumul_reward)
-            summary_writer.add_summary(score, global_step=e)
-            summary_writer.flush()
+        # Simulate episode
+        while not b_terminal:
+            # Get action
+            action = self.get_next_action(curr_state)
+            
+            # Observe state
+            next_state, reward, b_terminal, _ = self.env.step(action)
 
-            # Display score
-            tqdm_e.set_description("Score: " + str(cumul_reward))
-            tqdm_e.refresh()
+            # Memory
+            portfolio.append(action)
 
-        return results
-
-    def save_weights(self, path):
-        path += '_LR_{}'.format(self.lr)
-        self.actor.save(path)
-        self.critic.save(path)
-
-    def load_weights(self, path_actor, path_critic):
-        self.critic.load_weights(path_critic)
-        self.actor.load_weights(path_actor)
+            # Update current state
+            curr_state = next_state
